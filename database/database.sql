0ef0f3d9819979877d65153ec2efa49b8fb56a66
KwesiNavilot <andrewskwesiankomahene@gmail.com>
2022-02-07 19:05:32 +0000

    Updated database migration

--- database/database.sql
+++ database/database.sql
@@ -1,15 +1,15 @@
+DROP DATABASE veer;
 CREATE DATABASE veer
     CHARACTER SET utf8
     COLLATE utf8_general_ci;
 USE veer;
 
-CREATE TABLE redirects
-(
-    id   INT PRIMARY KEY AUTO_INCREMENT,
-    request_from    VARCHAR NOT NULL,
-    ip_address    VARCHAR(15) NOT NULL,
-    device     VARCHAR(100) NOT NULL,
-    platform        VARCHAR(100) NOT NULL,
-    software      VARCHAR(100) NOT NULL,
+CREATE TABLE redirects (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    request_from varchar(200) NOT NULL,
+    ip_address varchar(15) NOT NULL,
+    device varchar(100) NOT NULL,
+    platform varchar(100) NOT NULL,
+    software varchar(100) NOT NULL,
     event_date TIMESTAMP
 );
